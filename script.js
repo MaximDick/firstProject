f8204a6b48213ada9a6dfaeb9e0edc70649d8cc5
Maxim Dick <maxim1994Barca@gmail.com>
2019-12-07 13:28:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// let answer = +prompt("18?", "yes" );
+// console.log(typeof (answer));
+//
+// console.log("arr " + "object");
+// console.log("arr " - "object");
+// console.log("arr " + " - object");
+// console.log(4 + +" -object");
+
+let incr = 10;
+let deinc = 10;
+
+console.log(incr++);
+console.log(deinc--);
+console.log(++incr);
+console.log(--deinc);
+console.log(5%2);
+console.log("2" == 2);
+console.log("2" === 2);
+ let isChecked = true,
+     isClose = false;
+ console.log(isChecked || isClose);
+
+ a = 6;
+ console.log(a);
+
